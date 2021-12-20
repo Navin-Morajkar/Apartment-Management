4f88af81a9ee3633684a60bcfc81d196d028850b
Saish <saisaipawar@gmail.com>
2021-12-20 09:32:30 +0530

    ErrFix

--- Apartment-Management/src/apartment/management/ApartmentManagement.java
+++ Apartment-Management/src/apartment/management/ApartmentManagement.java
@@ -18,7 +18,7 @@ public class ApartmentManagement extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/apartment_managment/MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
         
         Scene scene = new Scene(root);
         
